f2ba65230890a3cc2bfee80ffc980140be87d1f7
Your Name <you@example.com>
2025-03-03 13:41:05 -0800

    undos

--- app/lib/uploadthing.ts
+++
@@ -1,5 +0,0 @@
-import { generateReactHelpers } from "@uploadthing/react";
-import type { OurFileRouter } from "../api/uploadthing/core";
-
-export const { useUploadThing, uploadFiles } =
-  generateReactHelpers<OurFileRouter>(); 
